44456a4b9f31645bd8cbdaf9645f1bd1fecbf981
LuciAguiar <laaguiar@hotmail.com>
2026-08-09 19:32:03 -0300

    Add files via upload

--- App.py
+++ App.py
@@ -347,7 +347,7 @@ elif menu == "Dashboards Dados 2022 - 2024":
         },
         {
              "arquivo": "evolucao_influencia_ipv_labels.png",
-             "titulo": "Influência do IPV Atual nos Indicadores Do Ano Seguinte",
+             "titulo": "Correlação Entre o IPV e os Demais Indicadores",
              "explicacao": "O gráfico revela uma mudança estrutural na dinâmica que leva um aluno ao 'Ponto de Virada' (IPV). O destaque absoluto é a disparada da influência do **Indicador Psicopedagógico (IPP)**, cuja correlação saltou drasticamente para 0.75 em 2024, assumindo a liderança como o maior motor de transformação. O Engajamento (IEG) também ganhou tração (0.58), enquanto o peso do Desempenho Acadêmico (IDA) permaneceu estável. O insight para a equipe é claro: o salto definitivo de desenvolvimento do aluno não é mais tracionado isoladamente pelas notas, mas está profundamente dependente do suporte psicopedagógico e de sua dedicação ativa."
         }  
     ]
